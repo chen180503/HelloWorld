06996775c58484a55805fd0d48cf462932215885
chendr <chendr@2345.com>
2018-05-03 18:20:50 +0800

    数据乘3 再筛选出大于10的数

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -7,6 +7,7 @@ object Hello {
     Logger.getLogger("org").setLevel(org.apache.log4j.Level.ERROR)
     //set master 本机使用local 远程写ip;打包的时候去掉 setMaster 因为会在参数中指定
     val conf = new SparkConf().setAppName("Hello")
+      conf.setMaster("local")
 //      conf.setMaster("spark://172.17.20.231:7077")
     val sc = new SparkContext(conf)
     val rdd = sc.parallelize(List(1, 2, 3, 4, 5, 6)).map(_ * 3)
