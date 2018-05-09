666a03c91785b0315b11969cb868937ac95beb31
chendr <chendr@2345.com>
2018-05-09 09:42:56 +0800

    更新pom依赖

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -6,9 +6,9 @@ object Hello {
     //只显示错误日志
     Logger.getLogger("org").setLevel(org.apache.log4j.Level.ERROR)
     //set master 本机使用local 远程写ip;打包的时候去掉 setMaster 因为会在参数中指定
+    //只能在本地跑 无法设置远程 conf.setMaster("spark://172.17.20.231:7077") 这种写法不生效
     val conf = new SparkConf().setAppName("Hello")
-      conf.setMaster("local")
-//      conf.setMaster("spark://172.17.20.231:7077")
+
     val sc = new SparkContext(conf)
     val rdd = sc.parallelize(List(1, 2, 3, 4, 5, 6)).map(_ * 3)
     //求和
