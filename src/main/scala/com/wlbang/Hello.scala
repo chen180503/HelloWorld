a23b30386d53d019eba11a3e61301af690ed098b
chendr <chendr@2345.com>
2018-05-10 08:54:57 +0800

    更新logo info

--- src/main/scala/com/wlbang/Hello.scala
+++ src/main/scala/com/wlbang/Hello.scala
@@ -5,7 +5,7 @@ import org.apache.log4j.Logger
 object Hello {
   def main(args: Array[String]): Unit = {
     //只显示错误日志
-    Logger.getLogger("org").setLevel(org.apache.log4j.Level.ERROR)
+    Logger.getLogger("org").setLevel(org.apache.log4j.Level.INFO)
     //set master 本机使用local 远程写ip;打包的时候去掉 setMaster 因为会在参数中指定
     //只能在本地跑 无法设置远程 conf.setMaster("spark://172.17.20.231:7077") 这种写法不生效
     val conf = new SparkConf().setAppName("Hello").setMaster("local")
